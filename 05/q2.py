f6a34883f98f1ecb5305497c4f05696f07f8f2ed
Flynn, Jonathan <Jonathan.Flynn@experian.com>
2023-12-06 19:37:48 +0000

    d5q2

--- 05/q2.py
+++ 05/q2.py
@@ -7,82 +7,90 @@ def get_input():
     return raw_data
 
 
+def sort_dict(ind):
+    myKeys = list(ind.keys())
+    myKeys.sort()
+    return {i: ind[i] for i in myKeys}
+
+
 def parse(data):
     seeds = {}
-    sections = {
-        "seed-to-soil map": {},
-        "soil-to-fertilizer map": {},
-        "fertilizer-to-water map": {},
-        "water-to-light map": {},
-        "light-to-temperature map": {},
-        "temperature-to-humidity map": {},
-        "humidity-to-location map": {}
-    }
+    sections = {}
     pointer = ""
     for line in data:
         if not re.match("^\d", line.split(":")[0]):
             pointer = line.split(":")[0]
             if pointer == "seeds":
-                seedy = [
-                    int(seed)
-                    for seed in line.split(":")[1].split()
-                ]
-                tmp_map = {}
-                for i in range(0, len(seedy), 2):
-                    # print(i)
-                    # print(seedy[i], seedy[i]+seedy[i+1])
-                    tmp_map[seedy[i]] = seedy[i]+seedy[i+1]
-                    # rg = list(range(seedy[i], seedy[i]+seedy[i+1]))
-                    # seeds += rg
-
-                myKeys = list(tmp_map.keys())
-                myKeys.sort()
-                seeds = {i: tmp_map[i] for i in myKeys}
-
+                seedy = [int(seed) for seed in line.split(":")[1].split()]
+                seeds = {seedy[i]: (seedy[i]+seedy[i+1])-1 for i in range(0, len(seedy), 2)}
+                seeds = sort_dict(seeds)
+            elif pointer != "":
+                sections[pointer] = {}
             continue
         else:
             values = line.split()
             sections[pointer][int(values[1])] = {"destination":int(values[0]), "range": int(values[2])}
+    
     for section in sections:
-        myKeys = list(sections[section].keys())
-        myKeys.sort()
-        sorted_dict = {i: sections[section][i] for i in myKeys}
-        sections[section] = sorted_dict
+        sections[section] = sort_dict(sections[section])
+    locations = {}
+    
+    for section in sections:
+        previous_end = 0
+        locations[section] = {}
+        for location in sections[section]:
+            if location > previous_end:
+                locations[section][previous_end] = {"destination": previous_end, "range": location}
+            locations[section][location] = sections[section][location]
+            previous_end = sections[section][location]["range"] + location
+    
+    for location in locations:
+        num_locations = len(locations[location])-1
+        last_location_key = list(locations[location])[num_locations]
+        ranger = locations[location][last_location_key]["range"]
+        locations[location][last_location_key+ranger] = {"destination": last_location_key+ranger, "range": 99999999999}
 
-    return seeds, sections
+    return seeds, locations
 
 
-def get_destination(seeds, data):
-    loc = 9999999999999999
-    for seed in seeds:
-        print()
-        for section in data:
-            print(f"{seed}, ", end="")
-            for range in data[section]:
-                if seed >= range and seed < range + data[section][range]["range"]:
-
-                    seed = seed - range + data[section][range]["destination"]
-                    break
-        if seed < loc:
-            loc = seed
-    print()
-    print(loc)
+def breakout(start,end, loc):
+    retdict = {}
+    for this_start in loc:
+        this_range = loc[this_start]["range"]
+        this_dest = loc[this_start]["destination"]
+        this_end = this_range + this_start
+        if start >= this_end or end < this_start:
+            continue
+            
+        if end >= this_end:
+            retdict.update(breakout(this_end, end, loc))
+            end = this_end-1
+        retdict[start - this_start+this_dest] = end - this_start+this_dest
+        break
+    return(retdict)
 
 
 def get_best(seeds, data):
-    bob = list(data.keys())
-    bob.reverse()
-    print(bob)
-    for key in bob:
-        print(key)
+    lowest = 9999999999999999999999999999999
+    current = {"start": 0, "end": 0}
+    for key, value in seeds.items():
+        current = {key: value}
+        for location in data:
+            current_temp = {}
+            for start, end in current.items():
+                current_temp.update(breakout(start, end, data[location]))
+            current = current_temp
+        current_list = list(current.keys())
+        current_list.sort()
+        if current_list[0] < lowest:
+            lowest = current_list[0]
+    print(lowest)
 
 
 def main():
     raw_data = get_input()
     seeds, parsed_data = parse(raw_data)
-    # get_destination(seeds, parsed_data)
     get_best(seeds, parsed_data)
-
 
 
 if __name__ == "__main__":
