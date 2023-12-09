c4c1c8f65b2eee740a92906b19e091c413d85846
Flynn, Jonathan <Jonathan.Flynn@experian.com>
2023-12-09 23:01:43 +0000

    d9q1q2

---
+++ 09/q1.py
@@ -0,0 +1,51 @@
+def get_input():
+    with open("input.txt") as f:
+        raw_data = f.read().splitlines()
+    return raw_data
+
+
+def sort_dict(ind):
+    myKeys = list(ind.keys())
+    myKeys.sort(reverse=True)
+    return {i: ind[i] for i in myKeys}
+
+
+def main():
+    raw_data = get_input()
+    counter = 0
+    tracker = {
+        i: {"depth": {}, "value": 0}
+        for i in range(0,len(raw_data))
+    }
+    for h in range(0, len(raw_data)):
+        vals = [
+            int(i)
+            for i in raw_data[h].split()
+        ]
+        tracker[h]["depth"][0] = vals[-1]
+        found = False
+
+        diff_count = 1
+        while not found:
+            diff_track = []
+            for j in range(0,len(vals)-1):
+                diff_track.append(vals[j+1]-vals[j])
+            tracker[h]["depth"][diff_count] = diff_track[-1]
+            if len(set(diff_track)) == 1:
+                found = True
+            else:
+                diff_count += 1
+                vals = diff_track
+        sub_total = 0
+        tracker[h]["depth"] = sort_dict(tracker[h]["depth"])
+        for _, value in tracker[h]["depth"].items():
+            sub_total += value
+        tracker[h]["value"] = sub_total
+        counter += sub_total
+        
+    print(tracker)
+    print(counter)
+
+
+if __name__ == "__main__":
+    main()
